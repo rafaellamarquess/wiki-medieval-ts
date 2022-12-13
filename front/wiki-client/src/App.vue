d8a85ad995a114ad9c3ead56c7b11b560dcfa2dc
Rafaella <fariassrafaella@gmail.com>
2022-12-13 11:57:41 -0300

    dark mode

--- front/wiki-client/src/App.vue
+++ front/wiki-client/src/App.vue
@@ -7,11 +7,28 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from '@/components/Navbar.vue';
 import Footer from '@/components/Footer.vue';
 
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "body", //element to add attribute to
+  attribute: "theme", // attribute name
+  valueDark: "custom-dark", // attribute value for dark mode
+  valueLight: "custom-light", // attribute value for light mode
+});
+
+
+
+const toggleDark = useToggle(isDark);
+
 </script>
 
 <template>
    <div>
      <Navbar />
+
+     <p>Dark theme: {{isDark}}</p>
+     <button @click="toggleDark()"> Dark Mode </button>
+
     </div>
 
   <RouterView />
@@ -23,5 +40,9 @@ import Footer from '@/components/Footer.vue';
 </template>
 
 <style scoped>
+[theme="custom-dark"]  {
+  background: #3d3d3d;
+  color: rgb(236, 227, 227);
+}
 
 </style>
--- front/wiki-client/src/views/Home.vue
+++ front/wiki-client/src/views/Home.vue
@@ -45,10 +45,42 @@
 </div>
 
 
-    <div class="row">
-    <div class="list-group">
-      <div class="col-lg-12">
-        <div class="container text-center">
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="list-group col-md-5">
+
+  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-3">List group item heading</h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <small>And some small print.</small>
+  </a>
+
+
+  <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-3">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <small class="text-muted">And some muted small print.</small>
+  </a>
+
+
+  <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-3">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <small class="text-muted">And some muted small print.</small>
+  </a>
+</div>
+
+
+<div class="list-group col-md-5">
   <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-3">List group item heading</h5>
@@ -76,7 +108,7 @@
 </div>
 </div>
 </div>
-</div>
+
 
 </template>
 
